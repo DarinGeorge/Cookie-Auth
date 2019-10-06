6e3d1399fb718ec86f7fb1df98d905d85208e443
Darin George <darin.george30@gmail.com>
2019-10-05 23:27:50 -0400

    Initial - Built Backend Server API

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//app
+const app = express();
+
+//get routes
+const appRoutes = require('./routes/store');
+const authRoutes = require('./routes/auth');
+
+//db
+mongoose
+	.connect(process.env.DATABASE_LOCAL, {
+		//Change Database from either CLOUD or LOCAL depending on environment.
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(() => console.log('Database connection confirmed.'));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//cors
+if (process.env.NODE_ENV == 'development') {
+	app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+//routes middleware
+app.use('/api', appRoutes);
+app.use('/api', authRoutes);
+
+//routes
+app.get('/api', (req, res) => {
+	res.json({ time: Date().toString() });
+});
+
+//port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+	console.log(`Server launched and pointing to port ${port}`);
+});
