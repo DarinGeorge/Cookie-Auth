b66b3622591e6b49f366b9c478949183ee58ca7f
Darin George <55860383+DarinGeorge@users.noreply.github.com>
2019-10-06 16:42:42 -0400

    Added Runtime Configurations

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    publicRuntimeConfig: {
-        APP_NAME: 'ECOMMERCE',
-        API_DEV: 'http://localhost:8000/api'
-        PRODUCTION: false
-    }
-}
+	publicRuntimeConfig: {
+		APP_NAME: 'ECOMMERCE',
+		API_DEV: 'http://localhost:8000/api',
+		PRODUCTION: false
+	}
+};
